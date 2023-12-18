76191d33e9fad2a70a264c0af085e1eec957c8b4
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2023-12-17 19:31:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import virustotal 
+import googleSB
+import json
 
 with open('test_domains.txt','r') as q:
     domains = q.readlines()
     
 domains = [x.replace('\n','') for x in domains]
 
-vt = virustotal.VirusTotal()
-results = []
-for domain in domains:
-    results.append(vt.virustotal_url_request(domain))
+with open('api_config.txt','r') as f:
+    config = json.loads(f.read())
 
-vt.csv_writer(results)
+VT_URL = config['virustotal']['url_url']
+VT_KEY = config['virustotal']['key']
+
+GSB_URL = config['googlesafebrowser']['url']
+GSB_KEY = config['googlesafebrowser']['key']
+GSB_ID  = config['googlesafebrowser']['ID']
+
+vt = virustotal.VirusTotal(VT_KEY,VT_URL)
+results_vt = []
+for domain in domains:
+    results_vt.append(vt.virustotal_url_request(domain))
+
+vt.csv_writer(results_vt)
+
+results_gsb = []
+
+gsb = googleSB.GoogleSafeBrowsing(GSB_KEY,GSB_URL,GSB_ID)
+results_gsb = gsb.SafeBrowsing_request(domains)
+
+gsb.csv_writer(results_gsb)
 

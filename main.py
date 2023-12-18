e4e251bc5b7e162330bfeb2c0a95809a6717126f
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2023-12-17 20:58:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import virustotal 
 import googleSB
+import IPQS
 import json
-
-with open('test_domains.txt','r') as q:
-    domains = q.readlines()
-    
-domains = [x.replace('\n','').replace('[.]','.') for x in domains]
 
 with open('api_config.txt','r') as f:
     config = json.loads(f.read())
@@ -17,17 +13,32 @@ GSB_URL = config['googlesafebrowser']['url']
 GSB_KEY = config['googlesafebrowser']['key']
 GSB_ID  = config['googlesafebrowser']['ID']
 
-vt = virustotal.VirusTotal(VT_KEY,VT_URL)
-results_vt = []
-for domain in domains:
-    results_vt.append(vt.virustotal_url_request(domain))
+IPQS_URL = config['ipqualityscore']['url']
+IPQS_KEY = config['ipqualityscore']['key']
 
-vt.csv_writer(results_vt)
+def main():
+    with open('test_domains.txt','r') as q:
+        domains = q.readlines()
+    
+    domains = [x.replace('\n','').replace('[.]','.') for x in domains]
+    
+    vt = virustotal.VirusTotal(VT_KEY,VT_URL)
+    ipqs = IPQS.IPQS(IPQS_KEY,IPQS_URL) 
+    gsb = googleSB.GoogleSafeBrowsing(GSB_KEY,GSB_URL,GSB_ID)
 
-results_gsb = []
+    results_vt = []
+    results_ipqs = []
+    results_gsb = []
 
-gsb = googleSB.GoogleSafeBrowsing(GSB_KEY,GSB_URL,GSB_ID)
-results_gsb = gsb.SafeBrowsing_request(domains)
+    for domain in domains:
+        results_vt.append(vt.virustotal_url_request(domain))
+        results_ipqs.append(ipqs.IpQS_url_request(domain))
 
-gsb.csv_writer(results_gsb)
+    results_gsb = gsb.SafeBrowsing_request(domains)
 
+    ipqs.csv_writer(results_ipqs)
+    gsb.csv_writer(results_gsb)
+    vt.csv_writer(results_vt)
+
+if __name__=='__main__':
+    main()

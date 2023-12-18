2502323fcf10307005fcca20733ce7cefc7e4137
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2023-12-17 19:34:16 -0500

    Update googleSB.py

--- googleSB.py
+++ googleSB.py
@@ -17,7 +17,8 @@ class GoogleSafeBrowsing():
             "threatInfo":{
                 "threatTypes":[
                     "THREAT_TYPE_UNSPECIFIED",
-                    "MALWARE","SOCIAL_ENGINEERING",
+                    "MALWARE",
+                    "SOCIAL_ENGINEERING",
                     "UNWANTED_SOFTWARE",
                     "POTENTIALLY_HARMFUL_APPLICATION"],
                 "platformTypes":["ANY_PLATFORM"],

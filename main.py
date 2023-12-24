39c04fe8a417ea1674093808a0a9c8570b2a4b08
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2023-12-24 14:58:45 -0500

    Update main.py
    moved csv function to its own module

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import virustotal 
 import googleSB
 import IPQS
+import urlhaus
+import datapresentation
+import datetime
 import json
 
 with open('api_config.txt','r') as f:
@@ -16,29 +19,37 @@ GSB_ID  = config['googlesafebrowser']['ID']
 IPQS_URL = config['ipqualityscore']['url']
 IPQS_KEY = config['ipqualityscore']['key']
 
+
 def main():
     with open('test_domains.txt','r') as q:
         domains = q.readlines()
+        domains = [x.replace('\n','').replace('[.]','.') for x in domains]
     
-    domains = [x.replace('\n','').replace('[.]','.') for x in domains]
-    
+    date = datetime.datetime.now()
+
     vt = virustotal.VirusTotal(VT_KEY,VT_URL)
     ipqs = IPQS.IPQS(IPQS_KEY,IPQS_URL) 
     gsb = googleSB.GoogleSafeBrowsing(GSB_KEY,GSB_URL,GSB_ID)
+    uhaus = urlhaus.URLHaus()
 
-    results_vt = []
-    results_ipqs = []
-    results_gsb = []
+    gsb_r = gsb.SafeBrowsing_request(domains)
+
+    total_results = []
+    
 
     for domain in domains:
-        results_vt.append(vt.virustotal_url_request(domain))
-        results_ipqs.append(ipqs.IpQS_url_request(domain))
+       
+        vt_r = vt.virustotal_url_request(domain) 
+        uhaus_r = uhaus.query_urlhaus(domain)
+        ipqs_r = ipqs.IpQS_url_request(domain)
+        gsb_r_d = gsb_r[domain]
+        
+        total_results.append(vt_r | uhaus_r | ipqs_r | gsb_r_d)
 
-    results_gsb = gsb.SafeBrowsing_request(domains)
-
-    ipqs.csv_writer(results_ipqs)
-    gsb.csv_writer(results_gsb)
-    vt.csv_writer(results_vt)
+    fieldnames = ['url','ip_address_ipqs','server_ipqs','dns_valid_ipqs','unsafe_ipqs','severity_vt_cs','safe_browsing_hit','response_vt','status_code_ipqs','url_status_uh','Forcepoint ThreatSeeker','Sophos','Xcitium Verdict Cloud','spamhaus_dbl','surbl','category_ipqs','risk_score_ipqs','domain_rank_ipqs','neutral_vt','bad_vt','not_seen_vt','times_submitted_vt','date_added_uh','threat_uh','tags_uh','age_ipqs','parking_ipqs','spamming_ipqs','malware_ipqs','phishing_ipqs','suspicious_ipqs','adult_ipqs','redirected_ipqs','content_type_ipqs','platforms_gsb','threats_gsb','cache_gsb','title_vt_cs','timestamp_vt_cs','details_vt_cs']
+    filename = f"{date.strftime(r'%B')}_{date.strftime(r'%d')}_{date.strftime(r'%H')}_total_results.csv"
+    
+    datapresentation.DataPresentation(fieldnames,filename,total_results).csv_writer()
 
 if __name__=='__main__':
     main()

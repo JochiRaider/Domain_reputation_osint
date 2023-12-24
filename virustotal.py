5db166b52eb408609cc6ef85d03ff5435e1bf2a6
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2023-12-24 15:00:13 -0500

    Update virustotal.py

--- virustotal.py
+++ virustotal.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import base64
-import csv
 import datetime
 
 
@@ -19,39 +18,38 @@ class VirusTotal:
             result = json.loads(response.text)
             result['data']['attributes']['categories'].setdefault('Forcepoint ThreatSeeker','')
             result['data']['attributes']['categories'].setdefault('Sophos','')
-            result['data']['attributes']['categories'].setdefault('BitDefender','')
             result['data']['attributes']['categories'].setdefault('Xcitium Verdict Cloud','')
+            not_good_hits = result['data']['attributes']['last_analysis_stats']['malicious'] + result['data']['attributes']['last_analysis_stats']['suspicious']
+            not_seen_hits = result['data']['attributes']['last_analysis_stats']['undetected'] + result['data']['attributes']['last_analysis_stats']['timeout']
             data = {
-                    'domain':domain,
-                    'times_submitted':result['data']['attributes']['times_submitted'],
-                    'response': '200',
-                    'reputation':result['data']['attributes']['reputation'],
-                    'harmless':result['data']['attributes']['last_analysis_stats']['harmless'],
-                    'malicious':result['data']['attributes']['last_analysis_stats']['malicious'],
-                    'suspicious':result['data']['attributes']['last_analysis_stats']['suspicious'],
-                    'undetected':result['data']['attributes']['last_analysis_stats']['undetected'],
-                    'timeout':result['data']['attributes']['last_analysis_stats']['timeout'],
+                    'url':domain,
+                    'times_submitted_vt':result['data']['attributes']['times_submitted'],
+                    'response_vt':'Found',
+                    'neutral_vt':result['data']['attributes']['last_analysis_stats']['harmless'],
+                    'bad_vt':not_good_hits,
+                    'not_seen_vt':not_seen_hits,
                     'Forcepoint ThreatSeeker':result['data']['attributes']['categories']['Forcepoint ThreatSeeker'],
                     'Sophos':result['data']['attributes']['categories']['Sophos'],
-                    'BitDefender':result['data']['attributes']['categories']['BitDefender'],
                     'Xcitium Verdict Cloud':result['data']['attributes']['categories']['Xcitium Verdict Cloud']
                     }
+            try:
+                crowdsourced_context = result['data']['attributes']['crowdsourced_context'][0]
+                timestamp = crowdsourced_context['timestamp']
+                timestamp = datetime.datetime.fromtimestamp(timestamp)
+                title = result['data']['attributes']['crowdsourced_context'][0]['source'] + ' , ' + result['data']['attributes']['crowdsourced_context'][0]['title']
+                data['title_vt_cs'] = title
+                data['timestamp_vt_cs'] = timestamp.strftime("%B %d, %Y, %I:%M %p")
+                data['details_vt_cs'] = result['data']['attributes']['crowdsourced_context'][0]['details']
+                data['severity_vt_cs'] = result['data']['attributes']['crowdsourced_context'][0]['severity']
+            except:
+                pass
         else:
             data = {
-                    'domain':domain,
-                    'response':'404'
+                    'url':domain,
+                    'response_vt':'Not Found'
                     }
         return data
     
-    def csv_writer(self,data:list[dict]):
-        fieldnames = ['domain','response','reputation','harmless','malicious','suspicious','undetected','timeout','times_submitted','Forcepoint ThreatSeeker','Sophos','BitDefender','Xcitium Verdict Cloud']
-        date = datetime.datetime.now()
-        filename = f"{date.strftime(r'%B')}_{date.strftime(r'%d')}_{date.strftime(r'%H')}_virustotal.csv"
-        with open(filename,'w',newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, restval='')
-            writer.writeheader()
-            writer.writerows(data)
-
 
 def main():
     pass
